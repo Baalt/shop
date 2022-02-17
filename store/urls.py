d66518e1231232ce4a76c8e1aac93ba2c890ecc8
Oleg Shchukin <enjoylifebalt@gmail.com>
2022-02-17 21:28:21 +0300

    - added templates - added static files - added routing - added test controllers

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from  .views import index
+from  .views import index, shop, glasses, contact, about
 
 urlpatterns = [
     path('', index),
+    path('shop/', shop),
+    path('glasses/', glasses),
+    path('contact/', contact),
+    path('about/', about),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'store/index.html', {})
+    return render(request, 'store/index.html')
+
+
+def shop(request):
+    return render(request, 'store/shop.html')
+
+
+def glasses(request):
+    return render(request, 'store/glasses.html')
+
+
+def contact(request):
+    return render(request, 'store/contact.html')
+
+
+def about(request):
+    return render(request, 'store/about.html')
